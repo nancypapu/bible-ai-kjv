15fc4ac3711dac095aeebad56d491ede6c67c907
nancypapu <65409861+nancypapu@users.noreply.github.com>
2026-01-02 18:56:30 +0530

    Update download_kjv.py

--- download_kjv.py
+++ download_kjv.py
@@ -1,1 +1,14 @@
+import requests
+import json
+
+URL = "https://raw.githubusercontent.com/thiagobodruk/bible/master/json/en_kjv.json"
+
+print("Downloading KJV Bible...")
+resp = requests.get(URL)
+resp.raise_for_status()
+
+with open("bible_kjv.json", "w", encoding="utf-8") as f:
+    json.dump(resp.json(), f, ensure_ascii=False)
+
+print("✅ KJV Bible downloaded")
 
